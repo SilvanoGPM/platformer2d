8532b04dd82fa1c724eadc4c81d106d6f2a30d53
SkyG0D <silvanosilvino@hotmail.com>
2022-08-16 21:37:56 -0300

    feat: add basic HUD

--- Scripts/Items.gd
+++ Scripts/Items.gd
@@ -1,15 +1,17 @@
 extends Area2D
 
-const ANIMATION_COLLECTED_NAME: String = 'collected'
+export var points = 1
 
 func _ready() -> void:
 	var _body_entered = connect('body_entered', self, '_on_body_entered')
 	var _animation_end = $animation.connect('animation_finished', self, '_on_animation_finished')
 	
 func _on_body_entered(_body: Node) -> void:
-	if not $animation.assigned_animation == ANIMATION_COLLECTED_NAME:
-		$animation.play(ANIMATION_COLLECTED_NAME)
+	GameManager.fruits += points
+	
+	if not $animation.assigned_animation == 'collected':
+		$animation.play('collected')
 
 func _on_animation_finished(animation_name: String) -> void:
-	if animation_name == ANIMATION_COLLECTED_NAME:
+	if animation_name == 'collected':
 		queue_free()
